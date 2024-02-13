08e7e23584f6cfcfdee785bdf959c58af91d9771
Vikram Naik <26vikramnaik@gmail.com>
2024-02-13 22:42:41 +0530

    Basic component setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,47 @@
+import styles from './style.ts'
+
+import {
+  Navbar,
+  Billing,
+  Business,
+  Button,
+  CardDeal,
+  Clients,
+  CTA,
+  FeedbackCard,
+  Footer,
+  GetStarted,
+  Hero,
+  Stats,
+  Testimonials,
+} from './components'
+
 const App = () => {
-  return <div>App</div>
+  return (
+    <main className="w-full overflow-hidden bg-primary">
+      <header className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </header>
+      <section className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </section>
+      <section className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </section>
+    </main>
+  )
 }
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx}'],
-  mode: 'jit',
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
